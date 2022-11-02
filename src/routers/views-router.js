cd23507453a336846e685c9c6e4d3af1516858af
dobbyjang0 <dobbyjang0@gmail.com>
2022-11-02 12:01:17 +0900

    mypage

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,7 +10,8 @@ const viewsRouter = express.Router();
 viewsRouter.use("/", serveStatic("home"));
 viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
-viewsRouter.use("/mypage", serveStatic("mypage"));
+viewsRouter.use("/mypage/info", serveStatic("mypage_info"));
+viewsRouter.use("/cart", serveStatic("cart"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
