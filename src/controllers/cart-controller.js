440132286593715f8f91abb95921c1ee419ca1d6
KeonU Kim <anythingthannothing@gmail.com>
2022-11-03 13:53:17 +0900

    feat(api): 카트 api 초안

--- src/controllers/cart-controller.js
+++ src/controllers/cart-controller.js
@@ -2,21 +2,24 @@ import { cartService } from '../services';
 
 const addToCart = async (req, res, next) => {
   const { userId, productId, quantity } = req.body;
-  const updatedCart = await cartService.addToCart(userId, productId, quantity);
-  return res.status(200).json(updatedCart);
+  const updatedCartItems = await cartService.addToCart(
+    userId,
+    productId,
+    quantity,
+  );
+  return res.status(200).json(updatedCartItems);
 };
 
 const deleteProduct = async (req, res, next) => {
   const { userId, productId } = req.body;
-  const updatedCart = await cartService.updateCart(userId, productId);
-  return res.status(200).json(updatedCart);
+  const updatedCartItems = await cartService.updateCart(userId, productId);
+  return res.status(200).json(updatedCartItems);
 };
 
 const clearCart = async (req, res, next) => {
   const { userId } = req.body;
-  await cartService.clearCart(userId);
-
-  return res.sendStatus(200);
+  const updatedCartItems = await cartService.clearCart(userId);
+  return res.status(200).json(updatedCartItems);
 };
 
 export { addToCart, deleteProduct, clearCart };
