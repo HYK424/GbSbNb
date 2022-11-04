edfe024f2e489dc36f5fea99980f61c7c9f73e7d
KeonU Kim <anythingthannothing@gmail.com>
2022-11-04 11:41:36 +0900

    feat: 비회원 장바구니 추가

--- src/views/product-detail/handle-cart.js
+++ src/views/product-detail/handle-cart.js
@@ -2,21 +2,26 @@ const minusBtn = document.querySelector('#minus');
 const plusBtn = document.querySelector('#plus');
 const cartBtn = document.querySelector('#addCart');
 const countElement = document.querySelector('#count');
+const cartNotification = document.querySelector('#cartNotification');
 
-const handleminusBtnClick = () => {
+cartBtn.addEventListener('click', handleCartBtnClick);
+minusBtn.addEventListener('click', handleminusBtnClick);
+plusBtn.addEventListener('click', handlePlusBtnClick);
+
+function handleminusBtnClick() {
   let count = Number(countElement.value);
   if (count <= 1) return;
   count--;
   countElement.value = count;
-};
+}
 
-const handlePlusBtnClick = () => {
+function handlePlusBtnClick() {
   let count = Number(+countElement.value);
   count++;
   countElement.value = count;
-};
+}
 
-const handleCartBtnClick = (event) => {
+function handleCartBtnClick() {
   const productId = window.location.pathname.split('/')[2];
   const quantity = Number(countElement.value);
   console.log(quantity);
@@ -33,14 +38,20 @@ const handleCartBtnClick = (event) => {
   if (itemIndex >= 0) {
     updatedCartItems[itemIndex].quantity += quantity;
   } else {
-    updatedCartItems.push({
-      productId: productId,
-      quantity: quantity,
-    });
+    updatedCartItems.push(newItem);
   }
   localStorage.setItem('cart', JSON.stringify(updatedCartItems));
-};
+  updateCartNotification(updatedCartItems.length);
+}
 
-cartBtn.addEventListener('click', handleCartBtnClick);
-minusBtn.addEventListener('click', handleminusBtnClick);
-plusBtn.addEventListener('click', handlePlusBtnClick);
+function updateCartNotification(cartItemsTotalCount) {
+  cartNotification.innerHTML = `
+    장바구니
+    <span
+      class="position-absolute top-1 start-100 translate-middle badge rounded-pill bg-danger"
+      id="cart-notification"
+    >
+      ${cartItemsTotalCount}
+      <span class="visually-hidden">unread messages</span>
+    </span>`;
+}
