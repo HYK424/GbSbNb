ce28266698db35e2ba1d713d716ff3d413d5a65a
HYK <turtle424@gmail.com>
2022-11-05 22:25:51 +0900

    feat: postCategory추가

--- src/views/adminPost/adminPost.js
+++ src/views/adminPost/adminPost.js
@@ -1,10 +1,14 @@
 const form = document.querySelector('#form');
-const titleInput = form.querySelector('.title');
-const categoryInput = form.querySelector('.category');
-const manufactureInput = form.querySelector('.manufacturer');
-const priceInput = form.querySelector('.price');
-const descriptionInput = form.querySelector('.description');
+const form2 = document.querySelector('#form2');
+const titleInput = form.querySelector('#title');
+const categoryInput = form.querySelector('#category');
+const manufactureInput = form.querySelector('#manufacturer');
+const priceInput = form.querySelector('#price');
+const descriptionInput = form.querySelector('#description');
 const thumbnailInput = form.querySelector('#formFile');
+
+const categoryNameInput = form2.querySelector('#categoryName');
+const categoryIdInput = form2.querySelector('#categoryId');
 let file;
 
 thumbnailInput.addEventListener('change', handleFiles, false);
@@ -12,14 +16,19 @@ function handleFiles() {
   file = this.files[0];
 }
 
+adminPostOrPut();
+
 async function adminPostOrPut() {
   if (!adminPostPath().length) {
     form.addEventListener('submit', adminPost);
+    form2.addEventListener('submit', categoryPost);
   } else {
     const product = await fetch(`http://localhost:3000/api/products/${adminPostPath()}`)
     const productData = await product.json();
     inputPosts(productData);
     form.addEventListener('submit', adminPut);
+    form2.style.display='none';
+    
   }
 }
 
@@ -54,6 +63,19 @@ function adminPostPath() {
   return window.location.pathname.split('/')[3];
 }
 
+async function handleGetCategories() {
+  const res = await fetch(/api/categories, {
+    method: 'GET',
+  });
+  const categories = await res.json();
+
+  categories.forEach((category) => {
+    console.log(category);
+  })
+
+  return categories;
+};
+
 //빈 input에 채우기
 function inputPosts(data) {
   titleInput.value = data.title;
@@ -61,6 +83,7 @@ function inputPosts(data) {
   manufactureInput.value = data.manufacturer;
   priceInput.value = data.price;
   descriptionInput.value = data.description;
+
   // thumbnailInput.value = productData.imageUrl; 보안 상 이유로 구현 불가
 }
 
@@ -90,4 +113,25 @@ async function adminPost(event) {
   }
 }
 
-adminPostOrPut();
+async function categoryPost(event) {
+  event.preventDefault();
+  const categoryid = categoryIdInput.value;
+  const categoryname = categoryNameInput.value;
+
+  try {
+    await fetch('/api/categories', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: categoryid,
+        name: categoryname,
+      })
+    }).then(reset());
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
