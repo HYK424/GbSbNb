330b271a01544ecae5e6958505a1c8f810bfaad5
dobbyjang0 <dobbyjang0@gmail.com>
2022-11-07 21:09:37 +0900

    feat:카트 수정중

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,30 +1,36 @@
-import * as cartDB from './cart_db.js';
-import * as cartTempDB from './cart_temp_db.js';
+import * as cartDB from "./cart_db.js";
+import * as cartTempDB from "./cart_temp_db.js";
 
 const itemCountAll = document.querySelector('#itemCountAll');
 const itemPriceAll = document.querySelector('#itemPriceAll');
 const totalPrice = document.querySelector('#totalPrice');
+const itemContainer = document.querySelector('#itemContainer');
 
 addAllElements();
 addAllEvents();
 
 function addAllElements() {
-  insertProductsfromCart();
+    insertProductsfromCart();
+}
+
+function addAllEvents() {
+    itemContainer.addEventListener('click', itemButtonEvent);
+    itemContainer.addEventListener("change", itemInputEvent);
 }
 
 async function insertProductsfromCart() {
-  const cartItems = cartDB.getItemAll();
-  for (let item of cartItems) {
-    let { productId, quantity } = item;
-    let itemGet = await Api.get('/api/products', productId);
-    let { price, title, imageUrl } = itemGet;
+    const cartItems = cartDB.getItemAll();
+    for (let item of cartItems) {
+        let { productId, quantity } = item;
+        let itemGet = await Api.get("/api/products", productId);
+        let { price, title, imageUrl } = itemGet;
 
-    const itemContainer = document.querySelector('#itemContainer');
-    const itemHTML = `<div class="item-div" id="productItem-${productId}">
+        const itemContainer = document.querySelector("#itemContainer");
+        const itemHTML = `<div class="item-div" id="productItem-${productId}">
         <label class="checkbox">
-            <input type="checkbox" id="checkbox-${productId}" checked>
+            <input type="checkbox" data-action="checkbox" data-id="${productId}" id="checkbox-${productId}" checked>
         </label>
-        <button class="delete-button" id="delete-${productId}">
+        <button class="delete-button" data-action="delete" data-id="${productId}" id="delete-${productId}">
             지우기
         </button>
         <img
@@ -35,51 +41,98 @@ async function insertProductsfromCart() {
         <p id="title-${productId}">${title}</p>
         <p id="unitPrice-${productId}">${price}</p>
         <p>원</p>
-        <button class="button" id="minus-${productId}" ${
-      quantity <= 1 ? 'disabled' : ''
-    }>
+        <button class="button" data-action="minus" data-id="${productId}" id="minus-${productId}" ${quantity <= 1 ? "disabled" : ""}>
             -
         </button>
-        <input class="input" id="quantityInput-${productId}" type="number" min="1" max="99" value="${quantity}"/>
-        <button class="button" id="plus-${productId}" ${
-      quantity >= 99 ? 'disabled' : ''
-    }>
+        <input class="input" data-action="quantityInput" data-id="${productId}" id="quantityInput-${productId}" type="number" min="1" max="99" value="${quantity}"/>
+        <button class="button" data-action="plus" data-id="${productId}" id="plus-${productId}" ${quantity >= 99 ? "disabled" : ""}>
             +
         </button>
         <p id="totalPrice-${productId}">${quantity * price}원</p>
     </div>`;
 
-    itemContainer.insertAdjacentHTML('beforeend', itemHTML);
-
-    document
-      .querySelector(`#delete-${productId}`)
-      .addEventListener('click', () => deleteItem(productId));
-
-    document
-      .querySelector(`#checkbox-${productId}`)
-      .addEventListener('change', () => toggleItem(productId));
-
-    document
-      .querySelector(`#plus-${productId}`)
-      .addEventListener('click', () => increaseItemQuantity(productId));
-
-    document
-      .querySelector(`#minus-${productId}`)
-      .addEventListener('click', () => decreaseItemQuantity(productId));
-
-    document
-      .querySelector(`#quantityInput-${productId}`)
-      .addEventListener('change', () => handleQuantityInput(productId));
-  }
+        itemContainer.insertAdjacentHTML("beforeend", itemHTML);
+    }
 }
 
-async function deleteItem(id) {
-  cartDB.deleteItem(id);
+function itemButtonEvent(e) {
+    let target = e.target;
+    if (target.tagName != 'BUTTON' || target.tagName != 'INPUT') return;
 
-  // 결제정보를 업데이트함.
+    let actionCase = target.dataset.action
+    let productId = target.dataset.id
+    let oldQuantity = 0
 
-  // 제품 요소(컴포넌트)를 페이지에서 제거함
-  document.querySelector(`#productItem-${id}`).remove();
 
-  // 전체선택 체크박스를 업데이트함
+    switch (actionCase) {
+        case 'delete':
+            cartDB.deleteItem(productId);
+            cartTempDB.deleteItem(productId);
+            document.querySelector(`#productItem-${productId}`).remove();
+            // 전체선택 체크박스를 업데이트함
+            break;
+        case 'minus':
+            oldQuantity = cartTempDB.getItem(productId).quantity
+
+            if (oldQuantity <= 1) break;
+
+            cartTempDB.updateItemQuantity(productId, oldQuantity - 1);
+
+            updateItemTotalPrice()
+
+            break;
+
+        case 'plus':
+            oldQuantity = cartTempDB.getItem(productId).quantity
+
+            if (oldQuantity >= 99) break;
+
+            cartTempDB.updateItemQuantity(productId, oldQuantity + 1);
+
+            updateItemTotalPrice()
+
+            break;
+    }
+
+    updateSummary()
+}
+
+function itemInputEvent(e) {
+    let target = e.target;
+    if (target.tagName != 'INPUT') return;
+
+    let actionCase = target.dataset.action
+    let productId = target.dataset.id
+
+    switch (actionCase) {
+        case "checkbox":
+            break;
+        case "quantityInput":
+            let newQuantity = target.value
+
+            if (newQuantity < 1 || newQuantity > 99) {
+                return alert("수량은 1~99 사이가 가능합니다.");
+            }
+
+            cartTempDB.updateItemQuantity(productId, newQuantity)
+
+            updateItemTotalPrice()
+            break;
+    }
+
+    updateSummary()
+}
+
+function updateSummary() {
+    itemCountAll.innerHTML = cartTempDB.getTotalCount()
+    itemPriceAll.innerHTML = cartTempDB.getTotalPrice()
+    totalPrice.innerHTML = cartTempDB.getTotalPrice() + 3000
+}
+
+function updateItemTotalPrice(productId) {
+    const itemTotalPriceInput = document.querySelector(`#totalPrice-${productId}`)
+    const item = cartTempDB.getItem(productId)
+    const totalPrice = item.quantity * item.price
+
+    itemTotalPriceInput.innerHTML = totalPrice
 }
