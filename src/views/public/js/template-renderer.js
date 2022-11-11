1554a7c917a6ec8ac08799df83a6694c4a41e28c
HYK424 <turtle424@naver.com>
2022-11-11 18:52:04 +0900

    fix: design

--- src/views/public/js/template-renderer.js
+++ src/views/public/js/template-renderer.js
@@ -152,12 +152,12 @@ function renderFooter() {
 }
 
 function getFooterHTML() {
-  return `<div class="row footerItem"><div class="w-100 d-flex justify-content-center align-items-center" style="margin: 0; padding: 0;">
+  return `<div class="d-flex justify-content-center align-items-center footerItem">
           <a class="mx-5 btn btn-dark opacity-75">프로젝트 소개</a>
                 <a class="mx-5 btn btn-dark opacity-75">공지사항</a>
                 <a class="mx-5 btn btn-dark opacity-75">입점 문의</a>
                 <a class="mx-5 btn btn-dark opacity-75">관리자 문의</a>
-              </div></div>`;
+              </div>`;
 }
 
 async function handleSearchSubmit(event) {
